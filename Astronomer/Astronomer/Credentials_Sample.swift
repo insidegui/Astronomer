0b9d0e1ee7e72478691bc3609719f0ceaad9d6ec
Guilherme Rambo <insidegui@gmail.com>
2016-11-10 20:23:34 -0200

    Added credentials

---
+++ Astronomer/Astronomer/Credentials_Sample.swift
@@ -0,0 +1,13 @@
+//
+//  Credentials.swift
+//  Astronomer
+//
+//  Created by Guilherme Rambo on 10/11/16.
+//  Copyright © 2016 Guilherme Rambo. All rights reserved.
+//
+
+import Foundation
+
+struct Credentials {
+    static let accessToken = "YOUR_PERSONAL_TOKEN_HERE"
+}
