c100308bdd2ddf2b977e1dac3e8b7a67c5fccf24
Guilherme Rambo <insidegui@gmail.com>
2016-11-16 14:58:08 -0200

    Created view model for repositories, using diff on repositories view ccontroller

--- Astronomer/Astronomer/RepositoriesTableViewController.swift
+++ Astronomer/Astronomer/RepositoriesTableViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import RxSwift
 
 class RepositoriesTableViewController: UITableViewController {
 
+    private struct Constants {
+        static let cellIdentifier = "repositoryCell"
+    }
+    
     private weak var provider: DataProvider!
+    
+    private let disposeBag = DisposeBag()
     
     private var user: User {
         didSet {
             update(with: user)
+        }
+    }
+    
+    private var repositoryViewModels: [RepositoryViewModel] = [] {
+        didSet {
+            tableView.reload(oldData: oldValue, newData: repositoryViewModels)
         }
     }
     
@@ -33,6 +46,16 @@ class RepositoriesTableViewController: UITableViewController {
         super.viewDidLoad()
 
         update(with: user)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+        
+        provider.repositories(by: user).observeOn(MainScheduler.instance).subscribe { event in
+            switch event {
+            case .next(let repositories):
+                self.repositoryViewModels = repositories.map(RepositoryViewModel.init)
+            default: break
+            }
+        }.addDisposableTo(self.disposeBag)
     }
     
     private func update(with user: User) {
@@ -40,76 +63,23 @@ class RepositoriesTableViewController: UITableViewController {
         title = UserViewModel(user: user).repositoriesTitle
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return repositoryViewModels.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = repositoryViewModels[indexPath.row].repository.name
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Astronomer/Astronomer/RepositoryViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  RepositoryViewModel.swift
+//  Astronomer
+//
+//  Created by Guilherme Rambo on 16/11/16.
+//  Copyright © 2016 Guilherme Rambo. All rights reserved.
+//
+
+import Foundation
+import IGListDiff
+
+extension Repository: Equatable { }
+
+final class RepositoryViewModel: NSObject {
+    
+    let repository: Repository
+    
+    init(repository: Repository) {
+        self.repository = repository
+        
+        super.init()
+    }
+    
+    override func diffIdentifier() -> NSObjectProtocol {
+        return repository.id as NSObjectProtocol
+    }
+    
+    override func isEqual(_ object: IGListDiffable?) -> Bool {
+        guard let other = object as? RepositoryViewModel else { return false }
+        
+        return other.repository == self.repository
+    }
+    
+}
+
+func ==(lhs: Repository, rhs: Repository) -> Bool {
+    return lhs.id == rhs.id
+        && lhs.name == rhs.name
+        && lhs.fullName == rhs.fullName
+        && lhs.description == rhs.description
+        && lhs.stars == rhs.stars
+        && lhs.forks == rhs.forks
+        && lhs.watchers == rhs.watchers
+        && lhs.owner == rhs.owner
+}
